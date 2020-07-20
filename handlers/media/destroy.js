188322e2fa8a0c7577683467dcc3b8bcd3be8c4c
Eko Teguh Widodo <id.ekoteguhw@gmail.com>
2020-07-21 01:21:56 +0900

    Integrasi dengan API Gateway untuk Delete Image

---
+++ handlers/media/destroy.js
@@ -0,0 +1,28 @@
+const apiAdapter = require('../../adapters/Api');
+
+const {
+    URL_MEDIA_SERVICE
+} = process.env;
+
+const api = apiAdapter(URL_MEDIA_SERVICE);
+
+module.exports = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const media = await api.delete(`/media/${id}`);
+        return res.json(media.data);
+    } catch (error) {
+        if (error.code === 'ECONNREFUSED') {
+            return res.status(500).json({
+                status: 'error',
+                message: 'Service unavailable'
+            })
+        }
+
+        const {
+            status,
+            data
+        } = error.response;
+        return res.status(status).json(data);
+    }
+}
--- handlers/media/index.js
+++ handlers/media/index.js
@@ -1,6 +1,8 @@
 const create = require('./create');
-const getAll = require('./getAll')
+const getAll = require('./getAll');
+const destroy = require('./destroy');
 module.exports = {
     create,
-    getAll
+    getAll,
+    destroy
 };
