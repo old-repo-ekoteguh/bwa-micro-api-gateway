c3b726a32ad3d56624b8ca9081938a6ffe76b57c
Eko Teguh Widodo <id.ekoteguhw@gmail.com>
2020-07-23 22:36:54 +0900

    Integrasi API Gateway untuk Update Member

--- routes/members.js
+++ routes/members.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const membersHandler = require('../handlers/members');
+const verifyToken = require('../middlewares/verifyToken');
 
 router.post('/register', membersHandler.register);
 router.post('/login', membersHandler.login);
+router.put('/', verifyToken, membersHandler.update);
 
 module.exports = router;
